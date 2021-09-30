c467f437f772a390af3091149456072167fa8bf0
GeethanjaliAnbalagan <84552111+GeethanjaliAnbalagan@users.noreply.github.com>
2021-10-01 01:47:11 +0530

    Update app.component.ts

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular 10 Spring Boot CRUD Full Stack App';
+  title = 'Employee Management System';
 }
